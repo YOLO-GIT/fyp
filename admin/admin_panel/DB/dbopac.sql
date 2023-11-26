5ddb38a405c2049c9cb5ba223dbdadefb8f80321
YOLO-GIT <ahmadsufi345@gmail.com>
2023-11-26 16:45:13 +0800

    DB Patches

--- admin/admin_panel/DB/dbopac.sql
+++ admin/admin_panel/DB/dbopac.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2023 at 12:52 PM
+-- Generation Time: Nov 26, 2023 at 09:44 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tblbook` (
-  `book_ID` int(25) NOT NULL,
+  `book_ID` varchar(25) NOT NULL,
   `book_title` varchar(255) NOT NULL,
   `book_author` varchar(25) NOT NULL,
   `book_ISBN` varchar(35) NOT NULL,
@@ -51,18 +51,20 @@ CREATE TABLE `tblbook` (
 --
 
 INSERT INTO `tblbook` (`book_ID`, `book_title`, `book_author`, `book_ISBN`, `publisher`, `book_dewey`, `book_category`, `book_desc`, `book_language`, `book_illustration`, `book_matter1`, `book_matter2`, `book_matter3`, `book_status`, `book_added`, `book_image`) VALUES
-(7578, 'Super Storymorie Certot#2 Sukan', 'Kumpulan Penulis', '978-967-411-759-7', 'PTS Publishing House Sdn ', '899.23 STO', 'Fiction', 'Antologi Cerita Kontol', '', '', '', '', '', '', '2023-10-23 12:49:19', 'uploads/Certot_3-Ayahku_Superhero.jpg'),
-(7574, 'Ilmu Keusahawanan Agrikultur', 'Suriatiyah', '978-967-371-054-6', 'Synergy Media Sdn Bhd', '338.1 SUR', 'Science', 'Berkaitan Agrikultur', '', '', '', '', '', '', '2023-10-23 12:21:32', 'uploads/Watermark.jpg'),
-(7575, 'Membuat Baja Kompos', 'L. Murbandono HS', '978-967-371-076-8', 'Synergy Media Sdn Bhd', '631.875 MUR', 'Science', 'Siri Agro itu Pertanian', '', '', '', '', '', '', '2023-10-23 12:26:11', 'uploads/default_book.jpg'),
-(7576, 'Misteri Disebalik Gerakan Solat 5 Waktu', 'H. Muhammad Nasir, MA', '978-967-048-281-1', 'Pustaka Al-Ehsan', '297.283 MUH', 'History', 'Nikmati Lazatnya Solat', '', '', '', '', '', '', '2023-10-23 12:30:45', 'uploads/default_book.jpg'),
-(7577, 'SAYA , SHERLOCK DAN LUPIN', 'Adler, Irene', '978-967-411-854-9', 'PTS Publishing House Sdn ', '823 IRE', 'Fiction', 'Babak terakhir opera', '', '', '', '', '', '', '2023-10-23 12:35:08', 'uploads/default_book.jpg'),
-(7573, 'Cina Muslim & Hubungannya dengan Alam Melayu', 'Yusuf Liu Baojun', '978-967-163-870-5', 'CM Diverse Sdn Bhd', '305.69 LIU', 'History', 'Buku berkaitan perkembangan islam', '', '', '', '', '', '', '2023-10-23 12:17:08', 'uploads/Cina-Muslim-dan-Hubungan-dengan-Alam-Melayu.jpg'),
-(7579, 'Super Robotik', 'Ahmad Nabil Musyiri', '978-967-411-882-2', 'PTS Publishing House Sdn ', '899.23 AHM', 'Fiction', 'Alumni Tunas Super', '', '', '', '', '', '', '2023-10-23 12:52:55', 'uploads/default_book.jpg'),
-(7580, 'Mr Gamer VS Miss H', 'Azim Hakimi', '978-967-411-806-8', 'PTS Publishing House Sdn ', '899.23 AZI', 'Fiction', 'Penulis Tunas Super', '', '', '', '', '', '', '2023-10-23 12:54:14', 'uploads/default_book.jpg'),
-(7581, 'Clash Of Crowns', 'Farhan Hakimi', '978-967-411-490-9', 'PTS Publishing House Sdn ', '899.23 FAR', 'Action', 'Clash Of Crowns', '', '', '', '', '', '', '2023-10-23 12:55:11', 'uploads/clash_of_the_crown_CO_correction.jpg'),
-(7582, 'Fairies of Carina ', 'Farrah Hana', '978-967-411-481-7', 'PTS Publishing House Sdn ', '899.23 FAR', 'Fiction', 'Penulis Tunas Super', '', '', '', '', '', '', '2023-10-23 12:56:05', 'uploads/default_book.jpg'),
-(7583, 'Pocket Time', 'Nur Erisha', '978-967-411-483-1', 'PTS Publishing House Sdn ', '899.23 NUR', 'Fiction ', 'Penulis Tunas Super', 'BM', 'Ya ', 'Malay fictio', '-', '-', 'Ada', '2023-10-29 23:38:22', 'uploads/default_book.jpg'),
-(7584, 'My Best Buddy', 'Sharina Elisa', '978-967-369-423-5', 'Bookiut', '899.23 SHA', 'Fiction ', '-', 'Bahasa M', 'Ya ', 'Malay fiction, Malay', '-', '-', 'Ada', '2023-10-30 01:30:54', 'uploads/default_book.jpg');
+('7578', 'Super Storymorie Certot#2 Sukan', 'Kumpulan Penulis', '978-967-411-759-6', 'PTS Publishing House Sdn ', '899.23 STO', 'Fiction', 'Antologi Cerita Kontol', '', '', '', '', '', '', '2023-10-23 12:49:19', 'uploads/Certot_3-Ayahku_Superhero.jpg'),
+('7574', 'Ilmu Keusahawanan Agrikultur', 'Suriatiyah', '978-967-371-054-6', 'Synergy Media Sdn Bhd', '338.1 SUR', 'Science', 'Berkaitan Agrikultur', '', '', '', '', '', '', '2023-10-23 12:21:32', 'uploads/Watermark.jpg'),
+('7575', 'Membuat Baja Kompos', 'L. Murbandono HS', '978-967-371-076-8', 'Synergy Media Sdn Bhd', '631.875 MUR', 'Science', 'Siri Agro itu Pertanian', '', '', '', '', '', '', '2023-10-23 12:26:11', 'uploads/default_book.jpg'),
+('7576', 'Misteri Disebalik Gerakan Solat 5 Waktu', 'H. Muhammad Nasir, MA', '978-967-048-281-1', 'Pustaka Al-Ehsan', '297.283 MUH', 'History', 'Nikmati Lazatnya Solat', '', '', '', '', '', '', '2023-10-23 12:30:45', 'uploads/default_book.jpg'),
+('7577', 'SAYA , SHERLOCK DAN LUPIN', 'Adler, Irene', '978-967-411-854-9', 'PTS Publishing House Sdn ', '823 IRE', 'Fiction', 'Babak terakhir opera', '', '', '', '', '', '', '2023-10-23 12:35:08', 'uploads/default_book.jpg'),
+('7573', 'Cina Muslim & Hubungannya dengan Alam Melayu', 'Yusuf Liu Baojun', '978-967-163-870-5', 'CM Diverse Sdn Bhd', '305.69 LIU', 'History', 'Buku berkaitan perkembangan islam', '', '', '', '', '', '', '2023-10-23 12:17:08', 'uploads/Cina-Muslim-dan-Hubungan-dengan-Alam-Melayu.jpg'),
+('7579', 'Super Robotik', 'Ahmad Nabil Musyiri', '978-967-411-882-2', 'PTS Publishing House Sdn ', '899.23 AHM', 'Fiction', 'Alumni Tunas Super', '', '', '', '', '', '', '2023-10-23 12:52:55', 'uploads/default_book.jpg'),
+('7580', 'Mr Gamer VS Miss H', 'Azim Hakimi', '978-967-411-806-8', 'PTS Publishing House Sdn ', '899.23 AZI', 'Fiction', 'Penulis Tunas Super', '', '', '', '', '', '', '2023-10-23 12:54:14', 'uploads/default_book.jpg'),
+('7581', 'Clash Of Crowns', 'Farhan Hakimi', '978-967-411-490-9', 'PTS Publishing House Sdn ', '899.23 FAR', 'Action', 'Clash Of Crowns', '', '', '', '', '', '', '2023-10-23 12:55:11', 'uploads/clash_of_the_crown_CO_correction.jpg'),
+('7582', 'Fairies of Carina ', 'Farrah Hana', '978-967-411-481-7', 'PTS Publishing House Sdn ', '899.23 FAR', 'Fiction', 'Penulis Tunas Super', '', '', '', '', '', '', '2023-10-23 12:56:05', 'uploads/default_book.jpg'),
+('7583', 'Pocket Time', 'Nur Erisha', '978-967-411-483-1', 'PTS Publishing House Sdn ', '899.23 NUR', 'Fiction ', 'Penulis Tunas Super', 'BM', 'Ya ', 'Malay fictio', '-', '-', 'Ada', '2023-10-29 23:38:22', 'uploads/default_book.jpg'),
+('7584', 'My Best Buddy', 'Sharina Elisa', '978-967-369-423-5', 'Bookiut', '899.23 SHA', 'Fiction ', '-', 'Bahasa M', 'Ya ', 'Malay fiction, Malay', '-', '-', 'Ada', '2023-10-30 01:30:54', 'uploads/default_book.jpg'),
+('7587', 'Sang Kancil', 'Kumpulan Penulis', '978-967-411-759-6', 'PTS Publishing House Sdn ', '899.23 STA', 'Fiction ', 'Testing', 'Bahasa Melayu', 'Ya ', 'Malay fiction', '', '', 'Ada', '2023-11-13 21:15:57', 'uploads/default_book.jpg'),
+('78431', 'Kisah Sang Hadi', 'Haikal ', '122-255-128-512-1', 'SMOworkshop', '123412312', 'History ', 'Kisah Sang Hadi bermula daripada tahun 2001', 'Bahasa Melayu', 'Ya ', '', '', '', 'Ada', '2023-11-14 11:11:12', 'uploads/default_book.jpg');
 
 -- --------------------------------------------------------
 
@@ -77,17 +79,10 @@ CREATE TABLE `tblbooking` (
   `user_ID` varchar(25) NOT NULL,
   `date_booked` date DEFAULT NULL,
   `time_booked` time DEFAULT NULL,
-  `status` varchar(25) NOT NULL
+  `status_ID` int(10) NOT NULL DEFAULT 0,
+  `status` varchar(25) NOT NULL,
+  `timestamp_column` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tblbooking`
---
-
-INSERT INTO `tblbooking` (`booking_ID`, `book_title`, `user_Name`, `user_ID`, `date_booked`, `time_booked`, `status`) VALUES
-('230002', 'Ilmu Keusahawanan Agrikultur', 'Ahmad Aziz', 'SJ0435', '2023-11-07', '05:53:42', 'isBooking'),
-('230001', 'Super Storymorie Certot#2 Sukan', 'Ahmad Aziz', 'SJ0435', '2023-11-07', '05:53:12', 'isBooking'),
-('230003', 'Membuat Baja Kompos', 'Ahmad Aziz', 'SJ0435', '2023-11-07', '05:54:02', 'isBooking');
 
 -- --------------------------------------------------------
 
@@ -151,15 +146,28 @@ INSERT INTO `tbllibrarians` (`librarians_ID`, `librarians_name`, `librarians_una
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblprofile`
+-- Table structure for table `tblrecord`
 --
 
-CREATE TABLE `tblprofile` (
-  `user_id` varchar(25) NOT NULL,
-  `bio` varchar(50) NOT NULL,
-  `birthday` varchar(10) NOT NULL,
-  `negeri` varchar(25) NOT NULL
+CREATE TABLE `tblrecord` (
+  `record_ID` varchar(25) NOT NULL,
+  `book_title` varchar(50) NOT NULL,
+  `user_ID` varchar(25) NOT NULL,
+  `user_name` varchar(35) NOT NULL,
+  `record_date` date DEFAULT NULL,
+  `record_time` time DEFAULT NULL,
+  `record_name` varchar(25) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblrecord`
+--
+
+INSERT INTO `tblrecord` (`record_ID`, `book_title`, `user_ID`, `user_name`, `record_date`, `record_time`, `record_name`) VALUES
+('230001', '', '', '', NULL, NULL, 'Borrowing'),
+('230001', '', '', '', NULL, NULL, 'Booking'),
+('230004', '', '', '', NULL, NULL, 'Borrowing'),
+('230004', '', '', '', NULL, NULL, 'Booking');
 
 -- --------------------------------------------------------
 
@@ -187,11 +195,35 @@ INSERT INTO `tblreport` (`report_ID`, `transc_ID`, `borrowing_ID`, `booking_ID`,
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tblreturning`
+--
+
+CREATE TABLE `tblreturning` (
+  `return_ID` varchar(25) NOT NULL,
+  `user_IC` varchar(35) NOT NULL,
+  `user_name` varchar(25) NOT NULL,
+  `book_title` varchar(35) NOT NULL,
+  `book_condition` varchar(25) NOT NULL,
+  `date_returned` date DEFAULT NULL,
+  `return_approval` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblreturning`
+--
+
+INSERT INTO `tblreturning` (`return_ID`, `user_IC`, `user_name`, `book_title`, `book_condition`, `date_returned`, `return_approval`) VALUES
+('230001', '031022140525', 'Sufi', 'Super Storymorie Certot#2 Sukan', 'Terbaik', '2023-11-21', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tblstudent`
 --
 
 CREATE TABLE `tblstudent` (
   `stud_ID` varchar(25) NOT NULL,
+  `stud_roles` varchar(9) NOT NULL,
   `stud_Name` varchar(35) NOT NULL,
   `stud_username` varchar(25) NOT NULL,
   `stud_Class` varchar(25) NOT NULL,
@@ -201,24 +233,20 @@ CREATE TABLE `tblstudent` (
   `verification_code` varchar(100) NOT NULL,
   `is_verified` tinyint(1) DEFAULT 0,
   `reset_token` varchar(25) NOT NULL,
-  `book_count` int(25) NOT NULL DEFAULT 0
+  `book_count` int(25) NOT NULL DEFAULT 0,
+  `bio` varchar(50) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `negeri` varchar(50) NOT NULL,
+  `report` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tblstudent`
 --
 
-INSERT INTO `tblstudent` (`stud_ID`, `stud_Name`, `stud_username`, `stud_Class`, `email`, `stud_pwd`, `date`, `verification_code`, `is_verified`, `reset_token`, `book_count`) VALUES
-('1', 'Sufi', '', 'Al - Arabi', '', '', NULL, '', 0, '', 0),
-('525', 'Sufi', '', '5 Al - Arabi', 'user@gmail.com', '3c9909afec', NULL, '', 0, '', 0),
-('0527', 'Sufi', '', '4 Al - Arabi', 'user@gmail.com', '3627909a29', NULL, '', 0, '', 0),
-('J0525', 'Sufi', '', '3 Al - Arabi', 'user@gmail.com', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', NULL, '', 0, '', 0),
-('B0522', 'Sufi&nbsp;Danial', '', '3 ABU', '', '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', '2023-10-15', '', 0, '', 0),
-('SJ0435', 'Ahmad Aziz', 'Sufi123', '5 Al - Arabi', 'ahmadsufi345@gmail.com', 'd9f7dfaf83ff1753bdd85e9cd77d18c876040a6f60f04b8330e76fdd198882612fd7439a2419e320395eea18406ccfadb65c7bc069cf8d94e9336a5813dfadcc', '2023-11-04', '4591ba', 1, '', 4),
-('SJ0417', 'Ahmad Aziz', 'Sufi1234', '5 Al - Arabi', 'ahmadsufi345@gmail.com', '7e5b9a6029b64c52cd6a6ad186653195b70cfb7770b6d71c3eb4ddf16f1e270f6852ee9c61d9a7f07f897459fcf2d68b0c2e6ad4ebc0a8f38efd72382e471176', '2023-11-04', 'aa0943', 1, '', 0),
-('SB0522', 'Mohd Saufi', 'Saufi125', '5 Al - Arabi', 'ahmadsufi345@gmail.com', '591f110c5f0e1bdce3ce99ee4d25a60959f343e90d120e4d473e3ccae5811d5d07cd991a64b6f18ef670b772d48adbb9c08206bb99e621b60046b3921b204b77', '2023-11-04', '478b98', 1, '', 0),
-('SJ0455', 'Ahmad Nasik', 'Nasik123', '3 Al - Arabi', 'ahmadsufi345@gmail.com', '15e62c3a41b7c6e3aa01d53ea6fe3db11427947991e70634b0480b1d48c8331839ec136cf7a28833b60dc9588f160ac57a299a72d271c58d52991bc6e9301d4b', '2023-11-04', '30b230', 1, '', 0),
-('SJ0467', 'Mohd Saufi', 'Saufi1235', '5 Al - Arabi', 'ahmadsufi345@gmail.com', '60919c8c5deb1bd7fd88002210b3317ea7157017385c1056b2d7e1d9936406bff0fc0bbf0a5690e73a10742e7e6b7f03cf8dc44b7eee0d777853dd92ea832b62', '2023-11-04', 'a32636', 1, '', 0);
+INSERT INTO `tblstudent` (`stud_ID`, `stud_roles`, `stud_Name`, `stud_username`, `stud_Class`, `email`, `stud_pwd`, `date`, `verification_code`, `is_verified`, `reset_token`, `book_count`, `bio`, `birthday`, `negeri`, `report`) VALUES
+('031022140525', '', 'Ahmad Sufi', 'user123', '3 ABU', 'alexandriad431@gmail.com', 'cdd994caa5e0aa64c607fc2e97e8b3c92d2475601574e6f2c66303bbd3374e1f640f9cbc5f6cfc2975ca99d0037c4eef326e31e9b9997c33ec6ef9c6e9150b84', '2023-11-16', '9f464d', 1, '', 15, 'Saya Student di SMK Tok Dor', '2009-06-16', 'Kelantan', 'Ada Buku terkoyak.'),
+('041222032456', 'Student', 'Ali Abu', 'Ali345', 'Kelas 3 AB', 'alexandriad431@gmail.com', 'cdd994caa5e0aa64c607fc2e97e8b3c92d2475601574e6f2c66303bbd3374e1f640f9cbc5f6cfc2975ca99d0037c4eef326e31e9b9997c33ec6ef9c6e9150b84', '2023-11-21', 'Sa50158', 1, '', 2, '', NULL, '', '');
 
 -- --------------------------------------------------------
 
@@ -228,6 +256,7 @@ INSERT INTO `tblstudent` (`stud_ID`, `stud_Name`, `stud_username`, `stud_Class`,
 
 CREATE TABLE `tblteachers` (
   `teachers_ID` varchar(25) NOT NULL,
+  `teacher_roles` varchar(9) NOT NULL,
   `teachers_Name` varchar(35) NOT NULL,
   `teachers_username` varchar(25) NOT NULL,
   `teachers_Password` varchar(255) NOT NULL,
@@ -236,15 +265,19 @@ CREATE TABLE `tblteachers` (
   `verification_code` varchar(100) NOT NULL,
   `is_verified` tinyint(1) NOT NULL,
   `reset_token` varchar(25) NOT NULL,
-  `book_count` int(25) NOT NULL DEFAULT 0
+  `book_count` int(25) NOT NULL DEFAULT 0,
+  `bio` varchar(50) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `negeri` varchar(50) NOT NULL,
+  `report` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tblteachers`
 --
 
-INSERT INTO `tblteachers` (`teachers_ID`, `teachers_Name`, `teachers_username`, `teachers_Password`, `email`, `date_teachers`, `verification_code`, `is_verified`, `reset_token`, `book_count`) VALUES
-('TJ0525', 'Mohd Saufi', 'Saufi123', 'd9e6762dd1c8eaf6d61b3c6192fc408d4d6d5f1176d0c29169bc24e71c3f274ad27fcd5811b313d681f7e55ec02d73d499c95455b6b5bb503acf574fba8ffe85', 'ahmadsufi345@gmail.com', '2023-11-06', '2aa822', 1, '', 0);
+INSERT INTO `tblteachers` (`teachers_ID`, `teacher_roles`, `teachers_Name`, `teachers_username`, `teachers_Password`, `email`, `date_teachers`, `verification_code`, `is_verified`, `reset_token`, `book_count`, `bio`, `birthday`, `negeri`, `report`) VALUES
+('031022140445', '', 'Danial Sufi', 'abu123', '3393f30f7162c1beed2a4ea2eb0e7bf5eadeabb6d00b91cbd481b516eecd1ae07750fc917e3ce86c49d2f3774ff6a7d772b08ade6861ce635c5649df84cb6b57', 'alexandriad431@gmail.com', '2023-11-18', 'T6f4c71', 1, '', 13, '', NULL, '', '');
 
 -- --------------------------------------------------------
 
@@ -253,48 +286,25 @@ INSERT INTO `tblteachers` (`teachers_ID`, `teachers_Name`, `teachers_username`, 
 --
 
 CREATE TABLE `tbltransaction` (
-  `transc_ID` int(25) NOT NULL,
+  `transc_ID` varchar(25) NOT NULL,
   `transc_name` varchar(35) NOT NULL,
-  `borrowing_ID` int(25) NOT NULL,
-  `stud_ID` int(35) NOT NULL,
-  `teachers_ID` int(25) NOT NULL,
-  `booking_ID` int(25) NOT NULL,
-  `transc_date` date DEFAULT NULL
+  `isBooked` tinyint(1) NOT NULL,
+  `book_ID` varchar(25) NOT NULL,
+  `book_title` varchar(255) NOT NULL,
+  `user_ID` varchar(25) NOT NULL,
+  `user_Name` varchar(25) NOT NULL,
+  `user_role` varchar(25) NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `time` timestamp NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbltransaction`
 --
 
-INSERT INTO `tbltransaction` (`transc_ID`, `transc_name`, `borrowing_ID`, `stud_ID`, `teachers_ID`, `booking_ID`, `transc_date`) VALUES
-(2, 'IsReturning', 0, 1, 0, 1, '0000-00-00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `userid` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userid`, `name`, `username`, `password`) VALUES
-(1, 'sopee', 'sopee123', 'sopeeddt'),
-(2, 'eaby', 'sahabat', 'password@1234'),
-(3, 'Azimah', 'bujim123', 'Azimhebat123'),
-(4, '', 'capybaraBlaster', '$2y$10$DCmkWa3PLNvWwc3anO2sLejNLrwoAXA.Md9H9Q6vCVGCmhzHUsz3.'),
-(5, '', 'doloh', '$2y$10$TSbGa4K5dDDUIDFHSL4bFeCi0wk8Nfw6HwgFTMCjf/.H43g3gdVwu'),
-(6, 'User 1', 'user1', 'userpass1'),
-(7, 'User 2', 'user2', 'userpass2'),
-(8, 'User 3', 'user3', 'userpass3');
+INSERT INTO `tbltransaction` (`transc_ID`, `transc_name`, `isBooked`, `book_ID`, `book_title`, `user_ID`, `user_Name`, `user_role`, `start_date`, `end_date`, `time`) VALUES
+('BR044575782023', 'Borrowing', 0, '7578', 'Super Storymorie Certot#2 Sukan', '031022140445', 'Danial Sufi', 'Teacher', '2023-11-24', '2023-11-24', '2023-11-24 15:05:12');
 
 --
 -- Indexes for dumped tables
@@ -331,16 +341,16 @@ ALTER TABLE `tbllibrarians`
   ADD PRIMARY KEY (`librarians_ID`);
 
 --
--- Indexes for table `tblprofile`
---
-ALTER TABLE `tblprofile`
-  ADD PRIMARY KEY (`user_id`);
-
---
 -- Indexes for table `tblreport`
 --
 ALTER TABLE `tblreport`
   ADD PRIMARY KEY (`report_ID`);
+
+--
+-- Indexes for table `tblreturning`
+--
+ALTER TABLE `tblreturning`
+  ADD PRIMARY KEY (`return_ID`);
 
 --
 -- Indexes for table `tblstudent`
@@ -361,20 +371,8 @@ ALTER TABLE `tbltransaction`
   ADD PRIMARY KEY (`transc_ID`);
 
 --
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userid`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `tblbook`
---
-ALTER TABLE `tblbook`
-  MODIFY `book_ID` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7585;
 
 --
 -- AUTO_INCREMENT for table `tblborrowing`
@@ -393,18 +391,6 @@ ALTER TABLE `tblheadstaff`
 --
 ALTER TABLE `tblreport`
   MODIFY `report_ID` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `tbltransaction`
---
-ALTER TABLE `tbltransaction`
-  MODIFY `transc_ID` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
