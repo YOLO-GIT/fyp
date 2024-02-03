69440d46957f13b0b16c784e24e7d4dff2f67093
YOLO-GIT <ahmadsufi345@gmail.com>
2024-02-03 22:24:26 +0800

    Old File Deletion

--- admin/admin_panel/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-// Destroy all session
-session_destroy();
-
-// redirect page to file "accomodation.php"
-echo "<script>window.location.href='../../LibraryPro/sbs-html/index.php';</script>";
